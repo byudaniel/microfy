a727f5f619ee36dc7df2372eb26b37c77b93df23
Daniel O'Neill <byudaniel@gmail.com>
2019-03-27 22:47:29 -0400

    Fix shortid call

--- src/nats-streaming.js
+++ src/nats-streaming.js
@@ -2,7 +2,11 @@ const NatsStreaming = require('node-nats-streaming')
 const shortid = require('shortid')
 
 function build({ stanClusterName = 'test-cluster', serviceName, natsUrl }) {
-  const stan = NatsStreaming.connect(stanClusterName, `${serviceName}-${shortid()}`, natsUrl)
+  const stan = NatsStreaming.connect(
+    stanClusterName,
+    `${serviceName}-${shortid.generate()}`,
+    natsUrl
+  )
   return stan
 }
 
